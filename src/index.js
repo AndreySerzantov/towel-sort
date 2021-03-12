c8225e973f5fe281bdcc78a6be03dc0adf9eb033
andrey <serzantovandreyiq@gmail.com>
2021-03-12 13:12:55 +0300

    fix: corrected an error in previously implemented functionality

--- src/index.js
+++ src/index.js
@@ -1,27 +1,7 @@
 
 // You should implement your task here.
 
-/*module.exports = function towelSort (matrix) {
-  let arr = [];
-
-  for(let i = 0; i < matrix.length; i++){
-      if(i % 2 == 0){
-          arr = arr.concat(matrix[i]);
-      }else{
-          arr = arr.concat(matrix[i].sort((a, b) => {return b - a}));
-      }
-  }
-
-  return arr;
-}*/
-
-let matrix = [
-  [ 1, 2, 3 ],
-  [ 4, 5, 6 ],
-  [ 7, 8, 9 ],
- ];
-
-function towelSort (matrix) {
+module.exports = function towelSort (matrix) {
   let arr = [];
 
   if(matrix == undefined || matrix.length == 0){
@@ -38,6 +18,4 @@ function towelSort (matrix) {
 
   return arr;
 }
-
-console.log(towelSort (matrix));
 
