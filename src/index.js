01c106d5329ddc30d61c7308efd838dfa55c3db4
andrey <serzantovandreyiq@gmail.com>
2021-03-12 13:18:43 +0300

    fix: corrected an error in previously implemented functionality

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ module.exports = function towelSort (matrix) {
   let arr = [];
 
   if(matrix == undefined || matrix.length == 0){
-    return 0;
+    return arr;
   }else{
     for(let i = 0; i < matrix.length; i++){
       if(i % 2 == 0){
