7a0865120028710de2cfc9072a965e71e98abd94
andrey <serzantovandreyiq@gmail.com>
2021-03-12 12:49:31 +0300

    feat: Implemented a new function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr = [];
+
+  for(let i = 0; i < array.length; i++){
+      if(i % 2 == 0){
+          arr = arr.concat(array[i]);
+      }else{
+          arr = arr.concat(array[i].sort((a, b) => {return b - a}));
+      }
+  }
+
+  return arr;
 }
+
